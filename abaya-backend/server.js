2fbbb5aeab2f7f44fc78a9f87cfb22b1f536a98e
Prajwal <vmprajwal5@gmail.com>
2026-03-13 20:39:42 +0530

    Fix backend server crash

--- abaya-backend/server.js
+++ abaya-backend/server.js
@@ -54,9 +54,9 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
